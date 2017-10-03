5a61eb6a8171c0ac74fd2a59eab33ba66d570e5d
Nishant Agrawal <nishant.102397@gmail.com>
2017-10-03 23:55:12 +0530

    set game.java to main class

--- tictactoe/src/tictactoe/Tictactoe.java
+++ tictactoe/src/tictactoe/Tictactoe.java
@@ -14,7 +14,7 @@ public class Tictactoe {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // TODO code application logic here
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -23,5 +23,5 @@ public class Tictactoe {
         });
         
     }
-    
+    */
 }
